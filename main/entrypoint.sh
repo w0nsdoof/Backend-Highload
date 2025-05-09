9f17ce04aaedd5de140a0b5346001e448ff230b8
Askar <w0nsdoof@gmail.com>
2025-05-09 22:49:30 +0500

    .

--- main/entrypoint.sh
+++ main/entrypoint.sh
@@ -12,4 +12,4 @@ mkdir -p /var/log/django
 touch /var/log/django/django.log
 
 # Start Gunicorn on the correct port
-exec gunicorn -w 3 -b 0.0.0.0:${GUNICORN_PORT:-8000} config.wsgi:application
+exec gunicorn -w 3 -b 0.0.0.0:${PORT:-8000} config.wsgi:application
